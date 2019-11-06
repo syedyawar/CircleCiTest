ad5e26148a2d4c9e85f7460e31e79faaf59fb1b6
syedyawar <yawar.hussain@atomicity-interactive.com>
2019-11-06 16:49:18 +0500

    build should be succeeded

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'circleCiProject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('circleTESTCiProject');
+    expect(app.title).toEqual('circleCiProject');
   });
 
   it('should render title', () => {
