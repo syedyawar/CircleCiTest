1bdfaa1ca4f7319f7e67db73ad2b8a2879326ea5
syedyawar <yawar.hussain@atomicity-interactive.com>
2019-11-06 15:50:42 +0500

    ng test fail mock

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'circleCiProject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('circleCiProject');
+    expect(app.title).toEqual('circleTESTCiProject');
   });
 
   it('should render title', () => {
